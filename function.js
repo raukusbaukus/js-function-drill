b5815fbfe83705fb2d2ae2a9bf1c169ac180bf07
bmatson <benjaminmatson@gmail.com>
2016-11-14 10:00:40 -0700

    ben adding functions

--- function.js
+++ function.js
@@ -3,5 +3,33 @@ var fn = {};
 // define functions on the fn object like this:
 // fn.functionName = function() { ... }
 // test with 'npm test'
+fn.nothing = function() {
+  return undefined;
+}
+
+fn.skiddoo = function () {
+  return '23 Skiddoo';
+}
+
+fn.emptyArray = function() {
+  return [];
+}
+
+fn.oddArray = function () {
+  return [1,3,5,7,9];
+}
+
+fn.fibArray = function() {
+  return [1,1,2,3,5,8,13];
+}
+
+fn.tenThousand = function() {
+  var arr = [];
+  for (i=0;i<10000;i++) {
+    arr.push(i+1);
+  }
+  return arr;
+}
+
 
 module.exports = fn;
